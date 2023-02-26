6d8c0be9552d5eae0fd78f5ae52364a12313d3b0
Vitalitak <kuzmenko.vo@phystech.edu>
2023-02-26 12:26:20 +0300

    Sweep equations

--- main.py
+++ main.py
@@ -38,8 +38,34 @@ def Pois(ne, ni, Ve, boxsize):
 
     return V
 
-def momentum(V):
+def momentum(V, uprev, boxsize, dt):
+    """
+        sweep method solution of momentum conservation equation
+        """
 
+    Nx = len(V)
+    dx = boxsize / Nx
+    u = [0 for k in range(0, Nx)]
+
+    # initialisation of sweeping coefficients
+    a = [0 for k in range(0, Nx)]
+    b = [0 for k in range(0, Nx)]
+
+    # forward
+    a[0] = -uprev[1] * dt / 4.0 / dx
+    b[0] = (V[1] - V[0])/dx - uprev[0]/dt
+
+    for i in range(1, Nx - 1):
+        a[i] = uprev[i+1] / 4.0 / dx / (-1 / dt + uprev[i - 1] * a[i-1] / 4.0 / dx)
+        b[i] = (-uprev[i-1] / 4.0 / dx * b[i - 1] + (V[i]-V[i-1])/dx - uprev[i] / dt) / (-1 / dt + uprev[i-1] * a[i-1] / 4.0 / dx)
+
+    a[Nx - 1] = 0
+    b[Nx - 1] = (-uprev[Nx-2] / 4.0 / dx * b[Nx-2] + (V[Nx-1]-V[Nx-2])/dx - uprev[Nx-1] / dt) / (-1 / dt + uprev[Nx-2] * a[Nx-2] / 4.0 / dx)  # boundary conditions for u (u[Nx-1]-u[Nx-2])
+
+    # backward
+    u[Nx - 1] = b[Nx - 1]
+    for i in range(Nx - 1, 0, -1):
+        u[i - 1] = a[i - 1] * u[i] + b[i - 1]
 
     return u
 
