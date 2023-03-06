cb0dbace2ccb4bedbdbe447e76491b2a0fd135dd
Vitalitak <kuzmenko.vo@phystech.edu>
2023-03-06 17:28:53 +0300

    Maxwell-Boltzmann electrons

--- four-orderRK.py
+++ four-orderRK.py
@@ -7,7 +7,7 @@ four order Runge-Kutta method for solution equation
 dy/dx=f(x, y)
 
 """
-def RKPois(h, y0, Nx)
+def RKPois(h, y0, Nx):
 
     y = [0 for k in range(0, Nx)]
 
@@ -16,3 +16,42 @@ def RKPois(h, y0, Nx)
 
 
     return y
+
+def main():
+
+    # initialisation of parameters
+    boxsize = 1000  # mkm
+    dt = 0.01  # ns
+    Nx = 1000
+    tEnd = 50  # ns
+    dne = 0.01
+    dni = 0.001
+    me = 1
+    mi = 70000
+    C = 1.4E-16
+    C /= 1.6E-19
+    Te = 2.3
+
+    # Te *= 1.7E12 / 9.1  # kT/me
+
+    Nt = int(tEnd / dt)
+    dx = boxsize / Nx
+    V = [0 for k in range(0, Nx)]
+    ne = [1 for k in range(0, Nx)]
+    ni = [1 for k in range(0, Nx)]
+    ue = [0 for k in range(0, Nx)]
+    ui = [-0.01 for k in range(0, Nx)]
+    Vrf = 0
+    Vdc = -10
+    # ne = dist_Bolt(V, 1, Te)
+
+    V = RKPois(dx, Vdc, Nx)
+
+    plt.plot(V)
+    plt.ylabel('V')
+    plt.show()
+
+    return 0
+
+if __name__ == "__main__":
+        main()
--- main.py
+++ main.py
@@ -205,7 +205,6 @@ def main():
     plt.show()
     #print(ni)
 
-
     return 0
 
 if __name__ == "__main__":
