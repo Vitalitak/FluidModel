02c50c40787c57f38113c8f233a20cf7cd76d58a
Vitalitak <kuzmenko.vo@phystech.edu>
2023-03-21 00:29:47 +0300

    plasma boundary conditions for numerical calculation of initial conditions

--- four-orderRK.py
+++ four-orderRK.py
@@ -25,9 +25,9 @@ def RKPois(h, y0, Nx):
 def main():
 
     # initialisation of parameters
-    boxsize = 1.5E-4  # m
+    boxsize = 3E-4  # m
     dt = 0.1  # ns
-    Nx = 100000
+    Nx = 1000000
     tEnd = 50  # ns
     dne = 0.01
     dni = 0.001
@@ -38,12 +38,14 @@ def main():
     Te = 2.3  # eV
     Ti = 0.06  # eV
     Vdc = -18
-    V0 = -0.01
+    #V0 = -0.01
     e = 1.6E-19
     n0 = 1E16  # m-3
     eps0 = 8.85E-12
     kTi = Ti * 1.6E-19  # J
     kTe = Te * 1.6E-19  # J
+    #V0 = -0.01
+    V0 = -1/2*m.sqrt(Te/2/Ti)*kTe/e/m.sinh(m.sqrt(e*e*n0/2/eps0/kTi)*boxsize-m.sqrt(Te/2/Ti))
 
     # Te *= 1.7E12 / 9.1  # kT/me
 
@@ -62,6 +64,8 @@ def main():
         uipl[i] = n0 * m.sqrt(kTi/mi) / Nipl[i]
         Nepl[i] = n0 * m.exp(e*Vpl[i]/kTe)
 
+    print(m.sqrt(eps0*kTe/e*e*n0))
+    print(V0)
 
     V = [0 for k in range(0, Nx)]
     ne = [1 for k in range(0, Nx)]
