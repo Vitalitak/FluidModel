6acd1cc903c861f6b703924f883a5fb41e1f49b1
Vitalitak <kuzmenko.vo@phystech.edu>
2023-03-21 17:03:30 +0300

    plasma boundary conditions for numerical calculation of initial conditions

--- four-orderRK.py
+++ four-orderRK.py
@@ -25,7 +25,7 @@ def RKPois(h, y0, Nx):
 def main():
 
     # initialisation of parameters
-    boxsize = 1.36E-4  # m
+    boxsize = 2E-4  # m
     dt = 0.1  # ns
     Nx = 1000000
     tEnd = 50  # ns
@@ -40,13 +40,13 @@ def main():
     Vdc = -18
     e = 1.6E-19
     n0 = 1E16  # m-3
+    a = 1.3E-4  # m
     P = 1.4  #  P = ni(a)/n0
     eps0 = 8.85E-12
     kTi = Ti * 1.6E-19  # J
     kTe = Te * 1.6E-19  # J
     #V0 = -0.01
-    #V0 = -1/2*m.sqrt(Te/2/Ti)*kTe/e/m.sinh(m.sqrt(e*e*n0/2/eps0/kTi)*boxsize-m.sqrt(Te/2/Ti))
-    V0 = kTe / e * (1 - P) / (m.cosh(m.sqrt(e * e * n0 / 2 / eps0 / kTi) * boxsize) - 1)
+    V0 = kTe / e * (1 - P) / (m.cosh(m.sqrt(e * e * n0 / 2 / eps0 / kTi) * a) - 1)
 
     # Te *= 1.7E12 / 9.1  # kT/me
 
@@ -61,7 +61,10 @@ def main():
     uipl = [0 for k in range(0, Nx)]
     dKsidx = [0 for k in range(0, Nx)]
     #V0 = [0 for k in range(0, Nx)]
-    for i in range(0, Nx):
+
+    Npl = int(a/dx)
+
+    for i in range(0, Npl):
         #V0[i] = kTe / e * (1 - 1.4) / (m.cosh(m.sqrt(e * e * n0 / 2 / eps0 / kTi) * (x[i]+dx)) - 1)
         Vpl[i] = V0*(1+2*Ti/Te*(m.cosh(m.sqrt(e*e*n0/2/eps0/kTi)*x[i])-1))
         Nipl[i] = n0 - n0*e*V0/kTe*(m.cosh(m.sqrt(e*e*n0/2/eps0/kTi)*x[i])-1)
@@ -96,6 +99,10 @@ def main():
 
     plt.plot(x, dKsidx)
     plt.ylabel('E')
+    plt.show()
+
+    plt.plot(x, uipl)
+    plt.ylabel('u')
     plt.show()
 
     return 0
