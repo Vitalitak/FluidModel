6f644c10ce440a3c83ad3b329d26b3eece32137c
Vitalitak <kuzmenko.vo@phystech.edu>
2023-06-26 11:59:43 +0300

    dynamic self-consistent system

--- dynamicSelfCons.py
+++ dynamicSelfCons.py
@@ -1,11 +1,7 @@
 import math as m
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.linalg import solve
-from scipy.integrate import quad
-# from sympy import Function, dsolve, Eq, Derivative, exp, symbols
-# from sympy.solvers.ode.systems import dsolve_system
-from pynverse import inversefunc
+
 
 """
 1D Fluid model of collisionless Ar plasma sheath
@@ -188,8 +184,70 @@ def momentum(V, n, uprev, kTi, kTe, n0, Nel, Nsh, Nx, dt):
 
     return u
 
+def momentum_e(V, n, uprev, kTi, kTe, n0, Nel, Nsh, Nx, dt):
+    """
+    Explicit conservative upwind scheme
+    """
+    # dt = 1E-11  # s
+    dx = 1E-5
+    e = 1.6E-19
+    me = 9.11E-31  # kg
+    gamma = 1
+    # u = [0 for k in range(0, Nx)]
+    u = np.zeros(Nx)
 
-def continuity(u, nprev, V, n0, kTe, nuiz, Nel, Nsh, Nx, dt):
+    # Psi = [0 for k in range(0, Nel)]
+    # N = [0 for k in range(0, Nel)]
+    Psi = np.zeros(Nx)
+    N = np.zeros(Nx)
+
+
+    Psi = e * V / kTe
+    N = n / n0
+
+    """
+    # initialisation of sweeping coefficients
+    a = [0 for k in range(0, Nel)]
+    b = [0 for k in range(0, Nel)]
+
+    # forward
+    # boundary conditions on plasma surface: (du/dx)pl = 0
+    #a[0] = -uprev[1] * dt / 4.0 / dx
+    #b[0] = (V[1] - V[0])/dx/m - uprev[0]/dt
+    a[0] = 1
+    b[0] = 0
+
+    for i in range(1, Nel - 1):
+        a[i] = -uprev[i+1]*dt / 4.0 / dx / (1 - uprev[i - 1]*dt * a[i-1] / 4.0 / dx)
+        b[i] = (uprev[i-1]*dt / 4.0 / dx * b[i - 1] - kTe/mi*dt*(Psi[i+1]-Psi[i]) /dx - kTi/mi*dt*m.pow(N[i], gamma-2)*(N[i+1]-N[i])/dx + uprev[i]) / (1 - uprev[i - 1]*dt * a[i-1] / 4.0 / dx)
+
+    # boundary condition on electrode surface: (du/dx)el = 0
+    a[Nel - 1] = 0
+    #b[Nx - 1] = (-uprev[Nx-2] / 4.0 / dx * b[Nx-2] + (V[Nx-1]-V[Nx-2])/dx - uprev[Nx-1] / dt) / (-1 / dt + uprev[Nx-2] * a[Nx-2] / 4.0 / dx)  # boundary conditions for u (u[Nx-1]-u[Nx-2])
+    #b[Nx - 1] = 0  # (u)p = 0
+    b[Nel - 1] = b[Nel - 2]/(1 - a[Nel - 2])  # (du/dx)el = 0
+
+    # backward
+    u[Nel - 1] = b[Nel - 1]
+    for i in range(Nel - 1, 0, -1):
+        u[i - 1] = a[i - 1] * u[i] + b[i - 1]
+    """
+
+    # Explicit conservative upwind scheme
+
+    u[0:Nsh] = uprev[0:Nsh]
+    # print(Psi[Nsh-1:Nel-1])
+    # print(Psi[Nsh:Nel])
+    # print(N[Nsh:Nel] ** (gamma - 2))
+    u[Nsh:Nel] = uprev[Nsh:Nel] + dt * (
+                kTe / me * (Psi[Nsh:Nel] - Psi[Nsh - 1:Nel - 1]) / dx - kTe / me * (N[Nsh:Nel] ** (gamma - 2)) * (
+                N[Nsh:Nel] - N[Nsh - 1:Nel - 1]) / dx - (
+                            uprev[Nsh:Nel] * uprev[Nsh:Nel] - uprev[Nsh - 1:Nel - 1] * uprev[Nsh - 1:Nel - 1]) / dx)
+
+    return u
+
+
+def continuity(u, nprev, ne, nuiz, Nel, Nsh, Nx, dt):
     """
     Explicit conservative upwind scheme
     """
@@ -257,27 +315,31 @@ def continuity(u, nprev, V, n0, kTe, nuiz, Nel, Nsh, Nx, dt):
     """
     # n[Nsh:Nel] = nprev[Nsh:Nel] - dt * ((nprev[Nsh:Nel]*u[Nsh:Nel]-nprev[Nsh-1:Nel - 1]*u[Nsh-1:Nel-1])/dx)
     n[Nsh:Nel] = nprev[Nsh:Nel] - dt * (
-            (nprev[Nsh:Nel] * u[Nsh:Nel] - nprev[Nsh - 1:Nel - 1] * u[Nsh - 1:Nel - 1]) / dx - nuiz * n0 * m.e ** (
-                e * V[Nsh:Nel] / kTe))
+            (nprev[Nsh:Nel] * u[Nsh:Nel] - nprev[Nsh - 1:Nel - 1] * u[Nsh - 1:Nel - 1]) / dx - nuiz * ne[Nsh:Nel])
 
     # print(((nprev[3] - nprev[2]) * u[3] + (u[3] - u[2]) * nprev[3]))
     return n
 
 
-def concentration_e(V, kTe, n0, Nel, Nx):
+def concentration_e(u, nprev, nuiz, Nel, Nsh, Nx, dt):
     """
-    Boltzmann distribution for electrons
+    Continuity equation for electrons
     """
 
     dx = 1E-5
     e = 1.6E-19
     # n = [0 for k in range(0, Nx)]
     n = np.zeros(Nx)
+
+    n[0:Nsh] = nprev[0:Nsh]
     """
-    for i in range(0, Nel):
-        n[i] = n0 * m.exp(e*V[i]/kTe)
+    for i in range(Nsh, Nel):
+        n[i] = nprev[i] - dt * ((nprev[i]*u[i]-nprev[i-1]*u[i-1])/dx)
+        #print(((nprev[i]-nprev[i-1])*u[i]+(u[i]-u[i-1])*nprev[i]))
     """
-    n[0:Nel] = n0 * m.e ** (e * V[0:Nel] / kTe)
+    # n[Nsh:Nel] = nprev[Nsh:Nel] - dt * ((nprev[Nsh:Nel]*u[Nsh:Nel]-nprev[Nsh-1:Nel - 1]*u[Nsh-1:Nel-1])/dx)
+    n[Nsh:Nel] = nprev[Nsh:Nel] - dt * (
+            (nprev[Nsh:Nel] * u[Nsh:Nel] - nprev[Nsh - 1:Nel - 1] * u[Nsh - 1:Nel - 1]) / dx - nuiz * nprev[Nsh:Nel])
 
     return n
 
@@ -312,7 +374,7 @@ def main():
     Arf = 0
     w = 13560000  # Hz
 
-    Nt = 0
+    Nt = 400
 
     print(Nt)
     print(int((Nper - 2) / w / dt))
@@ -418,30 +480,27 @@ def main():
 
 
     q = 0
-    # Vel = V[Nel-1] - 10 * m.sin(13560000*2*m.pi*dt)+q
     Vel = V[Nel - 1] + q
 
     V_1 = Pois(ne, ni, V, Vel, n0, dx, Nel, Nsh, Nx)
     # ui_1 = momentum(V_1, ni, ui, kTi, kTe, n0, Nel, Nsh, Nx, dt)
     ui_1 = momentum(V, ni, ui, kTi, kTe, n0, Nel, Nsh, Nx, dt)
-    # ue_1 = momentum_e(V_1, ne, ue, kTe, de, n0, Nel, Nx, dt)
-    # ni_1 = continuity(ui_1, ni, V_1, n0, kTe, nuiz, Nel, Nsh, Nx, dt)
-    ni_1 = continuity(ui, ni, V, n0, kTe, nuiz, Nel, Nsh, Nx, dt)
-    # ne_1 = concentration_e(V_1, kTe, n0, Nel, Nx)
-    ne_1 = concentration_e(V, kTe, n0, Nel, Nx)
-    # ne_1 = continuity(ue_1, ne, Nel, Nx, dt)
-    # q += e*(ni_1[Nel-1]*ui_1[Nel-1]-ne_1[Nel-1]*ue_1[Nel-1])*dt/C
+    ue_1 = momentum_e(V, ni, ui, kTi, kTe, n0, Nel, Nsh, Nx, dt)
+    ni_1 = continuity(ui, ni, ne, nuiz, Nel, Nsh, Nx, dt)
+    ne_1 = concentration_e(ue, ne, nuiz, Nel, Nsh, Nx, dt)
 
-    # electron current in diode model
 
+    """
     if V_1[Nel - 1] < 0:
         q += e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[0] * m.sqrt(3 * kTe / me) / 4 * m.exp(
             e * (V_1[Nel - 1] - V_1[0]) / kTe)) * dt / C
     else:
         q += e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[0] * m.sqrt(3 * kTe / me) / 4) * dt / C
+    """
+
+    q += e * (ni_1[Nel - 1] * ui_1[Nel - 1]-ne_1[Nel-1] * ue_1[Nel-1]) * dt / C
     VdcRF[0] = q
-    Iel[0] = e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[0] * m.sqrt(3 * kTe / me) / 4 * m.exp(
-        e * (V_1[Nel - 1] - V_1[0]) / kTe))
+    Iel[0] = e * (ni_1[Nel - 1] * ui_1[Nel - 1]-ne_1[Nel-1] * ue_1[Nel-1])
     Ii[0] = e * ni_1[Nel - 1] * ui_1[Nel - 1]
     VRF[0] = 0
     # print(e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[0]*m.sqrt(kTe/me)/4*m.exp(e*(V_1[Nel - 1]-V_1[0])/kTe)) * dt / C)
@@ -459,21 +518,23 @@ def main():
         V_2 = Pois(ne_1, ni_1, V_1, Vel2, n0, dx, Nel, Nsh, Nx)
         # ui_2 = momentum(V_2, ni_1, ui_1, kTi, kTe, n0, Nel, Nsh, Nx, dt)
         ui_2 = momentum(V_1, ni_1, ui_1, kTi, kTe, n0, Nel, Nsh, Nx, dt)
-        # ue_2 = momentum_e(V_2, ne_1, ue_1, kTe, de, n0, Nel, Nx, dt)
+        ue_2 = momentum_e(V_1, ne_1, ue_1, kTi, kTe, n0, Nel, Nsh, Nx, dt)
         # ni_2 = continuity(ui_2, ni_1, V_2, n0, kTe, nuiz, Nel, Nsh, Nx, dt)
-        ni_2 = continuity(ui_1, ni_1, V_1, n0, kTe, nuiz, Nel, Nsh, Nx, dt)
-        # ne_2 = concentration_e(V_2, kTe, n0, Nel, Nx)
-        ne_2 = concentration_e(V_1, kTe, n0, Nel, Nx)
-        # ne_2 = continuity(ue_2, ne_1, Nel, Nx, dt)
-        # q += e * (ni_2[Nel - 1] * ui_2[Nel - 1] - ne_2[Nel - 1] * ue_2[Nel - 1])*dt / C
+        ni_2 = continuity(ui_1, ni_1, ne_1, nuiz, Nel, Nsh, Nx, dt)
+        ne_2 = concentration_e(ue_1, ne_1, nuiz, Nel, Nsh, Nx, dt)
+
+        q += e * (ni_2[Nel - 1] * ui_2[Nel - 1] - ne_2[Nel - 1] * ue_2[Nel - 1])*dt / C
+
+        """
         if V_2[Nel - 1] < 0:
             q += e * (ni_2[Nel - 1] * ui_2[Nel - 1] - ne_2[0] * m.sqrt(3 * kTe / me) / 4 * m.exp(
                 e * (V_2[Nel - 1] - V_2[0]) / kTe)) * dt / C
         else:
             q += e * (ni_2[Nel - 1] * ui_2[Nel - 1] - ne_2[0] * m.sqrt(3 * kTe / me) / 4) * dt / C
+        """
+
         VdcRF[int(2 * i - 1)] = q
-        Iel[int(2 * i - 1)] = e * (ni_2[Nel - 1] * ui_2[Nel - 1] - ne_2[0] * m.sqrt(3 * kTe / me) / 4 * m.exp(
-            e * (V_2[Nel - 1] - V_2[0]) / kTe))
+        Iel[int(2 * i - 1)] = e * (ni_2[Nel - 1] * ui_2[Nel - 1] - ne_2[Nel - 1] * ue_2[Nel - 1])
         Ii[int(2 * i - 1)] = e * ni_2[Nel - 1] * ui_2[Nel - 1]
         VRF[int(2 * i - 1)] = - Arf * m.sin(w * 2 * m.pi * t)
         # print(e * (ni_2[Nel - 1] * ui_2[Nel - 1] - ne_2[0] * m.sqrt(3*kTe / me) / 4 * m.exp(
@@ -487,11 +548,10 @@ def main():
         V_1 = Pois(ne_2, ni_2, V_2, Vel3, n0, dx, Nel, Nsh, Nx)
         # ui_1 = momentum(V_1, ni_2, ui_2, kTi, kTe, n0, Nel, Nsh, Nx, dt)
         ui_1 = momentum(V_2, ni_2, ui_2, kTi, kTe, n0, Nel, Nsh, Nx, dt)
-        # ue_1 = momentum_e(V_1, ne_2, ue_2, kTe, de, n0, Nel, Nx, dt)
+        ue_1 = momentum_e(V_2, ne_2, ue_2, kTi, kTe, n0, Nel, Nsh, Nx, dt)
         # ni_1 = continuity(ui_1, ni_2, V_1, n0, kTe, nuiz, Nel, Nsh, Nx, dt)
-        ni_1 = continuity(ui_2, ni_2, V_2, n0, kTe, nuiz, Nel, Nsh, Nx, dt)
-        # ne_1 = concentration_e(V_1, kTe, n0, Nel, Nx)
-        ne_1 = concentration_e(V_2, kTe, n0, Nel, Nx)
+        ni_1 = continuity(ui_2, ni_2, ne_2, nuiz, Nel, Nsh, Nx, dt)
+        ne_1 = concentration_e(ue_2, ne_2, nuiz, Nel, Nsh, Nx, dt)
         # ne_1 = continuity(ue_1, ne_2, Nel, Nx, dt)
 
         """
@@ -501,17 +561,20 @@ def main():
         ui_1 = momentum(V_2, ni_1, ui_2, kTi, kTe, n0, Nel, Nx, dt)
         V_1 = Pois(ne_1, ni_1, Vel3, dx, Nel, Nx)
         """
-        # q += e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[Nel - 1] * ue_1[Nel - 1])*dt / C
+
+        q += e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[Nel - 1] * ue_1[Nel - 1])*dt / C
+
+        """
         if V_1[Nel - 1] < 0:
             q += e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[0] * m.sqrt(3 * kTe / me) / 4 * m.exp(
                 e * (V_1[Nel - 1] - V_1[0]) / kTe)) * dt / C
         else:
             q += e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[0] * m.sqrt(3 * kTe / me) / 4) * dt / C
+        """
 
         VdcRF[int(2 * i)] = q
         VRF[int(2 * i)] = - Arf * m.sin(w * 2 * m.pi * t)
-        Iel[int(2 * i)] = e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[0] * m.sqrt(3 * kTe / me) / 4 * m.exp(
-            e * (V_1[Nel - 1] - V_1[0]) / kTe))
+        Iel[int(2 * i)] = e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[Nel - 1] * ue_1[Nel - 1])
         Ii[int(2 * i)] = e * ni_1[Nel - 1] * ui_1[Nel - 1]
         # print(e * (ni_1[Nel - 1] * ui_1[Nel - 1] - ne_1[Nel - 1] * ue_1[Nel - 1])*dt / C)
 
@@ -551,7 +614,7 @@ def main():
 
     plt.plot(x, V, 'r')
     plt.plot(x, V_1, 'b')
-    plt.plot(x, V_2, 'm')
+    #plt.plot(x, V_2, 'm')
     # plt.plot(x, V_2, 'g')
     # plt.plot(x, V_3, 'm')
     plt.ylabel('V')
